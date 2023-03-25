35a605c3344c24972668469a82cf7de07db0365c
Michał Kubit <99410973+xxMANIAKxx@users.noreply.github.com>
2023-03-25 10:01:36 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,6 +59,21 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
